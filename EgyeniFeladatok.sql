37adc8a37fa7ebe0b98ff7c6f5e68e6c4f63f653
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2018-03-26 22:57:37 +0200

    Üzenet Csabinak + formátum javítása

--- EgyeniFeladatok.sql
+++ EgyeniFeladatok.sql
@@ -1,10 +1,10 @@
-Fehér Csaba:
+/* Fehér Csaba */
+/* NL: Csaba a megoldásokat is töltsed fel! */
 
+/* 1. Mennyit tett zsebre a tulaj 1995-ben, ha az összes költság a fizetések 10%-a? */
 
--- 1. Mennyit tett zsebre a tulaj 1995-ben, ha az összes költság a fizetések 10%-a?
+/* 2. Írja ki növekvő sorrendben, melyik fuvarozó mennyit keresett összesen! */
 
--- 2. Írja ki növekvő sorrendben, melyik fuvarozó mennyit keresett összesen!
+/* 3. Írja ki, mely termék fogyott jobban a 4 és 5-ös kódú terméknél 1995. első két negyedévében! */
 
--- 3. Írja ki, mely termék fogyott jobban a 4 és 5-ös kódú terméknél 1995. első két negyedévében!
-
--- 4. Mely termékek fogytak az átlag felett növekvő sorrendben? (a mennyiség mellett szerepeljen a db is) 
+/* 4. Mely termékek fogytak az átlag felett növekvő sorrendben? (a mennyiség mellett szerepeljen a db is)  */
