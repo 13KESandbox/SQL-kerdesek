73bdab87fc2f7f36dde60c36c81388981a7820c6
t103-09 <t103-09@jedlik.helyi>
2018-03-27 16:44:45 +0200

    update

--- KocsisGábor.sql
+++ KocsisGábor.sql
@@ -1,3 +1,7 @@
 /* valami */
 
-/* 1. Listázza ki a 3 legtöbbet fuvarozó cég, 3 leggyakoribb termékét. */
+/* Listázza ki az 5 legtöbbet fuvarozó cég, 3 leggyakoribb termékét! */
+
+/* Melyik az a 3 rendelés, amit a leghamarabb kiszállítottak?*/
+
+/* Mi volt a legnépszerűbb termék 1994-ben?*/
