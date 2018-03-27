b0faaa0d099d6d28af067f419cc8849cce53a324
t103-08 <t103-08@jedlik.helyi>
2018-03-27 16:56:20 +0200

    update

--- BakondiTamás.sql
+++ BakondiTamás.sql
@@ -2,3 +2,7 @@
 
 /*1. Rendezze csökkenő sorrendbe az irányító szám alapján, 
 hogy az alkalmazottak melyik fuvarozóval szállíttatott italokat.*/
+
+/*2. Csoportosítsa azokat a megrendeléseket amik raktáron vannak*/
+
+/*3. */
