83e72e9f7dbc46a4f898093b5ba00cc6b410061c
Smici95 <balint.schmidt@gmail.com>
2018-04-03 19:37:00 +0200

    update
    4ből 2 kérdés

--- SchmidtBálint.sql
+++ SchmidtBálint.sql
@@ -1,5 +1,34 @@
-/*valami*/
+/* Melyek azok a tejtermékek amelyek M-val kezdõdõ városba lettek kiszállítva Németországba?*/  
+  SELECT DISTINCT
+  t.Terméknév,
+  v.Város
+FROM rendelésrészletei rr
+  INNER JOIN rendelések r
+    ON rr.Rendeléskód = r.Rendeléskód
+  INNER JOIN vevõk v
+    ON r.Vevõkód = v.Vevõkód
+  INNER JOIN termékek t
+    ON rr.Termékkód = t.Termékkód
+  INNER JOIN kategóriák k
+    ON t.Kategóriakód = k.Kategóriakód
+WHERE k.Kategóriakód = 4
+AND v.Város LIKE 'M%'
+AND v.Ország LIKE 'Németország'
 
+/* 2.) Melyik termék volt  a legkelendõbb 1994. augusztusában? */ 
+  SELECT
+  t.Terméknév,
+  SUM(rr.Mennyiség) AS Mennyiség
+FROM rendelésrészletei rr
+  INNER JOIN termékek t
+    ON rr.Termékkód = t.Termékkód
+  INNER JOIN rendelések r
+    ON rr.Rendeléskód = r.Rendeléskód
+WHERE YEAR(r.RendelésDátuma) = 1994
+AND MONTH(r.RendelésDátuma) = 8
+GROUP BY t.Terméknév
+  ORDER BY Mennyiség DESC
+LIMIT 1
 
+/* 3.) 
 
-Ã¡
