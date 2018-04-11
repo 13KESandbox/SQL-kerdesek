c9b810187846ac6097605dd25b0b399ce2e2867c
nitslaszlo <nitslaszlo@gmail.com>
2018-04-11 15:53:14 +0200

    NL v1

--- EgyeniFeladatok.sql
+++ EgyeniFeladatok.sql
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
-függvény Összead(int a, int b)
-{
-    térj vissza a+b
-}
-/* Fehér Csaba */
-=======
-Függvény szoroz(int a, int b);
-/* Fehér Csaba */
 
-<<<<<<< HEAD
->>>>>>> a5cff5d155ea5e360ae3529992473a2ffc27b797
-/* NL: Csaba a megoldásokat is töltsed fel! */ 
-/* NL: Csaba a megoldásokat is töltsed fel!*/
+/* Nits László */
 
 /* 1. Mennyit tett zsebre a tulaj 1995-ben, ha az összes költság a fizetések 10%-a?*/
-Körte123
-Körte321
+SELEC1 
+
 /* 2. Írja ki növekvő sorrendben, melyik fuvarozó mennyit keresett összesen! */
-Alma
+SELEC2
 
 /* 3. Írja ki, mely termék fogyott jobban a 4 és 5-ös kódú terméknél 1995. első két negyedévében! */
+SELEC3
 
 /* 4. Mely termékek fogytak az átlag felett növekvő sorrendben? (a mennyiség mellett szerepeljen a db is)  */
+SELEC4
