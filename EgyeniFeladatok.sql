82d5962c527d6c1886607925c57872c893546e25
t103-05 <t103-05@jedlik.helyi>
2018-04-11 16:03:08 +0200

    KG_commitment

--- EgyeniFeladatok.sql
+++ EgyeniFeladatok.sql
@@ -1,14 +1,14 @@
 
-/* Tóth Gergő */
+/* Kocsis Gábor */
 
 /* 1. Mennyit tett zsebre a tulaj 1995-ben, ha az összes költság a fizetések 10%-a?*/
-SELEC1
+SELECT1
 
 /* 2. Írja ki növekvő sorrendben, melyik fuvarozó mennyit keresett összesen! */
-SELEC2
+SELECT2
 
 /* 3. Írja ki, mely termék fogyott jobban a 4 és 5-ös kódú terméknél 1995. első két negyedévében! */
-SELEC3
+SELECT3
 
 /* 4. Mely termékek fogytak az átlag felett növekvő sorrendben? (a mennyiség mellett szerepeljen a db is)  */
-SELEC4
+SELECT4
